e779745e901763b1a20dd1320f5e44e2aa61033c
Tom J <jellopancake213@gmail.com>
2026-07-26 02:33:30 +0800

    FInish 3totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,18 @@
-const totalIntegers = function() {
-  
+const contains = require("../2_contains/contains");
+
+const totalIntegers = function(items, total = 0) {
+    if(typeof(items) != 'object' || items == null) return;
+    for(let item of Object.values(items)){
+        if(Number.isInteger(item)){
+            total = total + 1;
+        }
+        if(typeof(item) == 'object' && item != null){
+           total = total + totalIntegers(item);
+        }
+    }
+    return total;
 };
-  
+const tite = totalIntegers([[1,2,3],[0,1,2,2,2]]);
+console.log(tite)  
 // Do not edit below this line
 module.exports = totalIntegers;
