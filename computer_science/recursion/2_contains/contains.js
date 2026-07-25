891a8c75c8d388dd34d9bb704e850d60ac3b0d49
Tom J <jellopancake213@gmail.com>
2026-07-25 22:38:53 +0800

    Finish contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,12 @@
-const contains = function() {
-  
-};
-  
+const contains = function(obj, find) {
+  for(let value of Object.values(obj)){
+    if(Object.is(value, find)) return true;
+    if(typeof(value) === 'object' && value != null){
+      if(contains(value, find)) return true;
+    }
+  }
+  return false;
+  }
+
 // Do not edit below this line
 module.exports = contains;
